3791bb81df41e9d304d8abfa81315a7b9809aba0
JungHyeok Choi <page1114@gamil.com>
2020-05-21 17:19:11 +0900

    Using use method

--- dapi/server.js
+++ dapi/server.js
@@ -7,15 +7,16 @@ var app = express();
 app.set('views',path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
-// app.use();
+app.use(bodyParser.json());
 
 app.get('/', function(request, response){
-    console.log(request);
+    console.log(request.body);
     response.render('index');
 });
+
 
 app.listen(8080, () => {
     var date = new Date();
     console.log("Server is Starting at http://localhost:8080 / " + 
-    date.toLocaleDateString() + " " + date.toLocaleTimeString());
+        date.toLocaleDateString() + " " + date.toLocaleTimeString());
 });
